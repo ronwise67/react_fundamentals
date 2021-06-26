49bc1c12b88f3ea420d302dca8ac0492aaa47a15
Ron Wise <ron@ronwisephoto.com>
2021-06-25 22:57:47 -0400

    keeping files up to date

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -2,37 +2,26 @@ import { Route, Link, Switch } from "react-router-dom";
 import Home from "./Home";
 import Resources from "./Resources";
 import FunctionalComponentDemo from "../concepts/FunctionalComponents/FunctionalComponentDemo";
+import JSXRules from "../concepts/JSXRules";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-list-styling">
         <ul className="sidebar-list list-unstyled">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/resources">Resources</Link>
-          </li>
-          <li>
-            <Link to="/functionalcomponent">Functional Component</Link>
-          </li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/resources">Resources</Link></li>
+          <li><Link to="/functionalcomponent">Functional Component</Link></li>
+          <li><Link to='jsxrules'>JSX Rules</Link></li>
         </ul>
       </div>
       <div className="sidebar-route">
         <Switch>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/resources">
-            <Resources />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/functionalcomponent">
-            <FunctionalComponentDemo />
-          </Route>
+          <Route exact path="/home"><Home /></Route>
+          <Route exact path="/resources"><Resources /></Route>
+          <Route exact path="/"><Home /></Route>
+          <Route exact path="/functionalcomponent"><FunctionalComponentDemo /></Route>
+          <Route exact path="/jsxrules"><JSXRules></JSXRules></Route>
         </Switch>
       </div>
     </div>
