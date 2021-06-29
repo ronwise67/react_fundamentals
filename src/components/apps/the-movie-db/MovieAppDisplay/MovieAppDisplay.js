a05bac88ea597c515e2d7322b5a2585ddd46c389
Ron Wise <ron@ronwisephoto.com>
2021-06-29 14:35:24 -0400

    Updated Movie App files

--- src/components/apps/the-movie-db/MovieAppDisplay/MovieAppDisplay.js
+++ src/components/apps/the-movie-db/MovieAppDisplay/MovieAppDisplay.js
@@ -2,14 +2,15 @@ import { tSTypeQuery } from '@babel/types'
 import { result } from 'lodash'
 import React from 'react'
 import { minVersion } from 'semver'
+import styled from 'styled-components'
 
 const MovieAppDisplay = ({movie}) => {
     return(
-        <div>
-            <img src={`http://image.tmdb.org/t/p/w500${movie.poster.path}`}/>
-            <h5>{movie.original_title}</h5>
-            <p>{movie.overview}</p>
-        </div>
+        <Movie>
+            <Poster src={`http://image.tmdb.org/t/p/w500${movie.poster.path}`}/>
+            <Title>{movie.original_title}</Title>
+            <Description>{movie.overview}</Description>
+        </Movie>
     )
 }
 
@@ -22,6 +23,34 @@ return(
         </div>
     </div>
 )
+
+const Movie = styled.div`
+    margin: 5px
+    width: 300px
+    height: 500px
+    border-radius: 5px
+    text-align: center
+    background: #48b1bf
+    `;
+
+const Poster = styled.img`
+    width: calc(100% - 20px)
+    height: calc(70% - 10px)
+    margin: 10px 10px 0 10px
+    border-radius: 5px
+    `
+    
+const Title = styled.h5`
+    margin: 5px 0 0 0
+    color: white
+    `
+
+const Description = styled.p`
+    height: 20%
+    color: white
+    overflow-wrap: break-word
+    overflow: scroll
+    `
 
 export default MovieAppDisplay
 
