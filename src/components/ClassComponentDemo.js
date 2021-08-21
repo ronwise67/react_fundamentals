d96e40e5f43293bbedd700e27058bed918bce874
Ron Wise <ron@ronwisephoto.com>
2021-08-21 15:19:20 -0400

    incremental push -- nothing exciting

---
+++ src/components/ClassComponentDemo.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+
+export default class ClassComponentDemo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { simpleMessage: "Welcome user!" }
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <div className="mainDiv">
+                    <h4>
+                        {this.state.simpleMessage}
+                    </h4>
+                </div>
+            </div>
+        )
+    }
+}
